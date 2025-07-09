bb71b67732b1d89a05ac5d7a560b27bdd72c7692
ngrluiz <luiz.nogueira4467@gmail.com>
2025-07-09 11:42:36 -0300

    Update

--- Java_Spring_Data/src/main/java/repository/UserRepository.java
+++ Java_Spring_Data/src/main/java/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package repository;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import DIO.Java_Spring_Data.User;
+import org.springframework.stereotype.Repository;
+import DIO.model.User; // Fixed import: use the correct package for User
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
